48d0838a2293811a5ee696aa65aae3aef1270aef
MrTorres30 <torresandelizw@gmail.com>
2026-08-25 18:44:59 -0400

    feat: wire up Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+using TaskManagerProC.Services;
+using TaskManagerProC.UI;
+var auditService = new AuditService();
+var taskManager  = new TaskManager(auditService);
+var ui           = new ConsoleInterface(taskManager, auditService);
+ui.Run();
